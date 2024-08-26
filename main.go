779653d67be991553e9ea2918dd1ca46569b2fa6
Eduardo Barroso <dev@edbar.xyz>
2024-08-26 19:54:52 -0300

    add: root exact matching and additional routes

--- main.go
+++ main.go
@@ -6,12 +6,27 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	// Makes exact matching in page root
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func showSnippets(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("These are your snippets"))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Let's make a new snippet"))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippets)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Starting server on :42069")
 	err := http.ListenAndServe(":42069", mux)
