8586941c066527bcd8412c9d11b1e558b8bc5389
Eduardo Barroso <dev@edbar.xyz>
2024-08-29 18:06:51 -0300

    feat: add fileserver on /static/

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,8 +8,11 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet?id=1", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	log.Println("Starting server on :42069")
 	err := http.ListenAndServe(":42069", mux)
