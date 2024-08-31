e7c42087e5600d3769f20ed510d4410ca6730ef3
Eduardo Barroso <dev@edbar.xyz>
2024-08-31 11:10:27 -0300

    feat: add --addr flag for server hosting URL
    The default configuration for the server is to be hosted on
    localhost:42069 (nice). The --addr flag may now be used when running the
    server in order to change the default address, be that port or hostname.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":42069", "HTTP network address for the server")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet?id=1", showSnippet)
@@ -14,7 +18,7 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting server on :42069")
+	log.Printf("Starting server on %s", *addr)
 	err := http.ListenAndServe(":42069", mux)
 	log.Fatal(err)
 }
