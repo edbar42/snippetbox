adff981a97bea47abcc9def006d143c1b4a7cdc0
Eduardo Barroso <dev@edbar.xyz>
2024-10-10 20:05:09 -0300

    feat(app): add user authentication

--- pkg/models/mysql/users.go
+++ pkg/models/mysql/users.go
@@ -33,7 +33,17 @@ func (m *UserModel) Insert(name, email, password string) error {
 }
 
 func (m *UserModel) Authenticate(email, password string) (int, error) {
-	return 0, nil
+	var id int
+	var hashedPassword []byte
+	row := m.DB.QueryRow("SELECT id, hashed_password FROM users WHERE email = ?", email)
+	err := row.Scan(&id, &hashedPassword)
+	if err == sql.ErrNoRows {
+		return 0, models.ErrInvalidCredentials
+	} else if err != nil {
+		return 0, err
+	}
+
+	return id, nil
 }
 
 func (m *UserModel) Get(name, email, password string) (*models.User, error) {
